085df534b3b9e1fb5a3948878c09b1c0de7aa22c
Alberto Otero <alberto211994@gmail.com>
2021-04-13 20:44:19 +0200

    Creaccion base de datos

---
+++ Base de datos/base.sql
@@ -0,0 +1,155 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dad
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `coordenadas`
+--
+
+DROP TABLE IF EXISTS `coordenadas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `coordenadas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `latlong` json NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `coordenadas`
+--
+
+LOCK TABLES `coordenadas` WRITE;
+/*!40000 ALTER TABLE `coordenadas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `coordenadas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `log` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_placa` int NOT NULL,
+  `fecha` datetime NOT NULL,
+  `asunto` longtext COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pk_placa` (`id_placa`),
+  CONSTRAINT `pk_placa` FOREIGN KEY (`id_placa`) REFERENCES `placa` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `log`
+--
+
+LOCK TABLES `log` WRITE;
+/*!40000 ALTER TABLE `log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `placa`
+--
+
+DROP TABLE IF EXISTS `placa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `placa` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `coordenadas` json NOT NULL,
+  `energiaMaxima` decimal(6,4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `placa`
+--
+
+LOCK TABLES `placa` WRITE;
+/*!40000 ALTER TABLE `placa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `placa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `placa_produccion`
+--
+
+DROP TABLE IF EXISTS `placa_produccion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `placa_produccion` (
+  `id_placa` int NOT NULL,
+  `id_sol` int NOT NULL,
+  `posicionServo` int NOT NULL,
+  `fecha` datetime NOT NULL,
+  `produccion` decimal(6,4) NOT NULL,
+  PRIMARY KEY (`id_placa`,`id_sol`),
+  KEY `pk_sol_idx` (`id_sol`),
+  CONSTRAINT `pk_placa_sol` FOREIGN KEY (`id_placa`) REFERENCES `placa` (`id`),
+  CONSTRAINT `pk_sol` FOREIGN KEY (`id_sol`) REFERENCES `posicionsol` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `placa_produccion`
+--
+
+LOCK TABLES `placa_produccion` WRITE;
+/*!40000 ALTER TABLE `placa_produccion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `placa_produccion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posicionsol`
+--
+
+DROP TABLE IF EXISTS `posicionsol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posicionsol` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_coordenadas` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_coordenadas` (`id_coordenadas`),
+  CONSTRAINT `fk_coordenadas` FOREIGN KEY (`id_coordenadas`) REFERENCES `coordenadas` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posicionsol`
+--
+
+LOCK TABLES `posicionsol` WRITE;
+/*!40000 ALTER TABLE `posicionsol` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posicionsol` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-13 20:35:02
